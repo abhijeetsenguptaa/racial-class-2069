ce20e2b58ab631e39385e1e5e0ec1359aa539f2e
Anandkr21 <anandkumargupta.akg15@gmail.com>
2023-03-29 23:49:57 +0530

    working in day-3

--- backend/server.js
+++ backend/server.js
@@ -24,28 +24,32 @@ let board = ['', '', '', '', '', '', '', '', ''];
 io.on("connection", (socket) => {
     console.log('socket connected')
 
-    if(users.length===2){
+
+    // checking if user is more than 2
+    if (users.length === 2) {
         socket.emit('full');
         return;
     }
 
-    if(users.length===0){
+    if (users.length === 0) {
         turn = 'x';
         board = ['', '', '', '', '', '', '', '', ''];
     }
 
 
     let user = {
-        id: socket.id,
+        id: socket.id, // random id generate by socket
         name: "Player" + (users.length + 1),
         symbol: users.length === 0 ? 'x' : 'o',
         number: (users.length + 1)
     };
 
-    if(users.length < 2){
+    // if user is one , push one more user into room
+    if (users.length < 2) {
         users.push(user)
     }
 
+    //emiting user into room with their name
     socket.emit('setUser', user)
 
     socket.emit('start', {
@@ -54,18 +58,21 @@ io.on("connection", (socket) => {
         board: board,
     })
 
-    socket.on('move', (data) =>{
+    // listen move from client side
+    socket.on('move', (data) => {
         board = data.board;
         turn = data.turn;
         io.emit('move', {
-            board:board,
+            board: board,
             turn: turn,
-            i : data.i,
+            i: data.i,
         });
     });
 
     console.log(user)
 
+
+    //server disconnection when any user leave
     socket.on('disconnect', () => {
         console.log('user Disconnected')
         users = users.filter((user) => user.id !== socket.id);
@@ -76,21 +83,21 @@ io.on("connection", (socket) => {
 
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("Welcome to the Live X's And O's")
 })
 
 // All the routes are going to be attached here..
-app.use('/users',userRoute);
+app.use('/users', userRoute);
 
 
 
 
-app.listen(process.env.port,async(req,res)=>{
-    try{
+app.listen(process.env.port, async (req, res) => {
+    try {
         await connection;
         console.log('Server is connected to the Database.');
-    }catch(err){
+    } catch (err) {
         console.log('Server could not connect to the Database.');
     }
     console.log(`Server is listening to the port : ${process.env.port}.`);
