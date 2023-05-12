12c28ec65c535e16d6c57c928689a89f1b9c1088
Anandkr21 <anandkumargupta.akg15@gmail.com>
2023-05-12 23:55:39 +0530

    singup signin modified

--- frontend/main.js
+++ frontend/main.js
@@ -10,62 +10,62 @@ let messageInput = document.getElementById('messageInput');
 let sendBtn = document.getElementById('sendBtn');
 
 
-socket.emit("userDetailsX",userDetail(name,room));
+socket.emit("userDetailsX", userDetail(name, room));
 
 socket.on("output", (msg) => {
-  showIncomingMessage(`${msg.bot}`,`${msg.message}`,`${msg.time}`)
+  showIncomingMessage(`${msg.bot}`, `${msg.message}`, `${msg.time}`)
 });
 
 socket.on("joinedGame", (data) => {
-  showIncomingMessage(data.bot,`${name}! ${data.message}`,data.time)
+  showIncomingMessage(data.bot, `${name}! ${data.message}`, data.time)
 });
 
-sendBtn.addEventListener("click",()=>{
-  socket.emit("message",messageFormat(name,message))
+sendBtn.addEventListener("click", () => {
+  socket.emit("message", messageFormat(name, message))
 })
 
 
 
-function showIncomingMessage(name, message,timeX) {
+function showIncomingMessage(name, message, timeX) {
   let displayMessage = document.getElementById("displayMessage");
   let messageBox = document.createElement("div");
-  messageBox.setAttribute('class',"messageBox");
+  messageBox.setAttribute('class', "messageBox");
   let identity = document.createElement("div");
-  identity.setAttribute('class',"identity");
+  identity.setAttribute('class', "identity");
   let user_name = document.createElement("h4");
   user_name.innerText = name;
   let time = document.createElement("h4");
   time.innerText = timeX;
-  identity.append(user_name,time);
+  identity.append(user_name, time);
   let mainMessage = document.createElement("div");
-  mainMessage.setAttribute('class',"mainMessage");
+  mainMessage.setAttribute('class', "mainMessage");
   let para = document.createElement("p");
   para.innerText = message;
   mainMessage.append(para);
-  messageBox.append(identity,mainMessage);
+  messageBox.append(identity, mainMessage);
   displayMessage.append(messageBox);
 }
 
 
 
-function userDetail(name,room){
+function userDetail(name, room) {
   return {
-    name,room
+    name, room
   }
 }
 
 
-function messageFormat(name,message){
+function messageFormat(name, message) {
   return {
     name,
-    message:messageInput.value
+    message: messageInput.value
   }
 }
 
 
 let logOutBtn = document.getElementById('logOutBtn');
 
-logOutBtn.addEventListener('click',()=>{
-  localStorage.setItem('token',null);
+logOutBtn.addEventListener('click', () => {
+  localStorage.setItem('token', null);
   window.location = "./index.html"
 })
